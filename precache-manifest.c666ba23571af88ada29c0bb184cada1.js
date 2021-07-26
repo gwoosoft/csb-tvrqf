e2d39d2d59823bc6e878916df9537b3c3d49e60d
Code Sandbox <hello@codesandbox.io>
2021-07-26 23:44:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c666ba23571af88ada29c0bb184cada1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee0c6aad61b32da861c74980ec8c548",
+    "url": "https://gwoosoft.github.io/csb-tvrqf/index.html"
+  },
+  {
+    "revision": "58595ec3bdbcfa991b35",
+    "url": "https://gwoosoft.github.io/csb-tvrqf/static/js/2.36582637.chunk.js"
+  },
+  {
+    "revision": "e247685afe42ce3ff7b6",
+    "url": "https://gwoosoft.github.io/csb-tvrqf/static/js/main.0cd3b5a2.chunk.js"
+  },
+  {
+    "revision": "8a3dda8802793df1914e",
+    "url": "https://gwoosoft.github.io/csb-tvrqf/static/js/runtime-main.e9050d5c.js"
+  }
+]);
